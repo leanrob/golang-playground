e96de10c12ac213a7fc5729d7a1ed58b2aa3941e
leanrob <rbyrne@xerafloptech.com>
2017-09-26 00:14:40 -0700

    Golang Playground
    - Loops, functions, switch statements, and more
    - Basics of Golang complete

--- main.go
+++ main.go
@@ -12,14 +12,120 @@ const (
 
 func main() {
 
-	 imaginaryNumbers()
+	 //OneRunAll();
 
-	 constants()
+	TwoRunAll()
 
-	 Arrays()
 
-	 maps()
+}
 
+func TwoRunAll() {
+
+	branching()
+
+	loops()
+
+	switches()
+}
+
+func switches() {
+
+	capacities := []float64{30, 30, 42, 420, 22, 66}
+
+	activePlants := []int{0, 1, 4}
+
+	fmt.Print("Give us a number to print:  (1 or 2)  ")
+
+	var numberToPrint string
+
+	fmt.Scan(&numberToPrint)
+
+	switch numberToPrint {
+	case "1":
+		for idx, cap := range capacities {
+			fmt.Printf("Plant %d capacity: %.0f\n ", idx, cap)
+		}
+	case "2":
+		capacity := 0.
+		for _, plantId := range activePlants {
+			capacity += capacities[plantId]
+			fmt.Println(capacity)
+		}
+	default:
+		fmt.Println("Unknown option")
+	}
+}
+
+
+func loops() {
+
+	m := make(map[string]string)
+
+	m["first"] = "foo"
+	m["second"] = "bar"
+	m["third"] = "bob"
+
+	for k, v := range m {
+		println(k, v)
+	}
+
+	s := []string{"one", "duece", "tres"}
+
+	for idx, v := range s {
+		println(v, idx)
+	}
+
+
+	bar := 1
+
+	for {
+		bar++
+		println(bar)
+
+		if bar > 420 {
+			break
+		}
+	}
+
+
+	foo := 10
+
+	for i:=0; i<foo; i++ {
+		println(i)
+	}
+}
+
+func branching() {
+	bar := 5
+
+	switch {
+	case bar == 1:
+		println(1)
+	case bar > 2:
+		println(2)
+	}
+
+
+	// Initialize the value to be tested right in the if statement
+	if foo := 2; foo == 1 {
+		println("yup")
+	} else {
+		println("not 1")
+	}
+}
+
+
+
+/* Section 1 */
+
+func OneRunAll() {
+	imaginaryNumbers()
+
+	constants()
+
+	Arrays()
+
+	maps()
 }
 
 func imaginaryNumbers() {
