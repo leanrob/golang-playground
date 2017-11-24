fddb25a68a8f1ee0adaa3aac779b871cf79245c1
leanrob <rbyrne@xerafloptech.com>
2017-11-24 15:50:17 -0800

    Serving a page from go api
    Serving a static page on the default localhost load
    API is running and working in this commit

---
+++ rest-api/people.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+
+)
+
+// The person Type (more like an object)
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
